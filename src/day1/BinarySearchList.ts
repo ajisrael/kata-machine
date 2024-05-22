652cf0eccf56eab06fbf1de35d2c06697079fedc
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-05-22 11:20:44 -0400

    f   - completed binary search algo

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,4 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-    // Assumed must be in order
+    // Assumed array must be in order
 
+    let lo = 0;
+    let hi = haystack.length;
+
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m];
+
+        if (v === needle) {
+            return true;
+        } else if (v < needle) {
+            lo = m + 1
+        } else {
+            hi = m
+        }
+    } while (lo < hi);
+    
+    return false;
 }

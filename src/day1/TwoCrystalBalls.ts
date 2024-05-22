29c2a58906fa2544f5ea3494f35acf631d3fdf77
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-05-22 12:34:43 -0400

    f   - completed two crystal balls algo

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,29 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const interval = Math.floor(Math.sqrt(breaks.length));
+    let i = 0;
+    let broke = false;
+
+    for (;i < breaks.length; i += interval) {
+        if (breaks[i]) {
+            broke = true;
+            break;
+        }
+    }
+
+    if (!broke) {
+        return -1;
+    }
+
+    i -= interval;
+    const offset = i;
+
+    for (; i <= offset + interval && i < breaks.length; i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
+
 }
